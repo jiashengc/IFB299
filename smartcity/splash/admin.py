3cee8b584d87a9fec3d6e993cb4fa5e0d4990325
CJ <jensen.jr@outlook.com>
2017-10-12 12:36:03 +1000

    Update admin.py

--- smartcity/splash/admin.py
+++ smartcity/splash/admin.py
@@ -16,3 +16,23 @@ class locationAdmin(admin.ModelAdmin):
 	)
 
 admin.site.register(Location, locationAdmin)
+
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'Profile'
+    fk_name = 'user'
+
+
+class CustomUserAdmin(UserAdmin):
+    inlines = (ProfileInline, )
+
+    def get_inline_instances(self, request, obj=None):
+        if not obj:
+            return list()
+        return super(CustomUserAdmin, self).get_inline_instances(request, obj)
+
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
