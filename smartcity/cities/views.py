5bb005cbd92fde5786b5680940f02be1c6f1bcc2
Jia Sheng Chong <jiashengc@outlook.com>
2017-10-31 18:57:01 +1000

    fix

--- smartcity/cities/views.py
+++ smartcity/cities/views.py
@@ -34,6 +34,7 @@ def locationType(request, cityName, locationType):
     foundCity[0] = models.City.objects.get(name=cityName)
     pk = foundCity[0].pk
 
+    if not checkPermission(locationType, request.user.profile.account_type, request):
         return HttpResponseForbidden()
 
     foundLocations = models.Location.objects.filter(city=pk).filter(type=locationType)
@@ -51,7 +52,7 @@ def location(request, cityName, locationType, location):
     foundCity[0] = models.City.objects.get(name=cityName)
     pk = foundCity[0].pk
 
-    if not checkPermission(locationType, request.user.profile.account_type):
+    if not checkPermission(locationType, request.user.profile.account_type, request):
         return HttpResponseForbidden()
 
     foundLocation = [1]
@@ -73,7 +74,10 @@ def compareLocations(request):
     })
 
 
-def checkPermission(locationType, accountType):
+def checkPermission(locationType, accountType, request):
+    if request.session['access'] == 1:
+        return True
+
     if accountType == 'Student':
         return locationType == 'CL'\
             or locationType == 'LI'
