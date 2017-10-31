ad7bcf53108795143398bd180107577229f48bf7
CJ <jensen.jr@outlook.com>
2017-10-31 16:37:49 +1000

    Update views.py

--- smartcity/splash/views.py
+++ smartcity/splash/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import HttpResponseRedirect
-from spash import models
+from splash import models
 
 # Create your views here.
 def index(request):
