c5f5a34451762199fa445ace791e76c5d414dd4a
CJ <jensen.jr@outlook.com>
2017-11-01 17:11:27 +1000

    Add code comments

--- smartcity/splash/admin.py
+++ smartcity/splash/admin.py
@@ -6,12 +6,13 @@ from .models import Profile
 from .models import Event
 # Register your models here.
 
-
+# Add admin options for Locations
 class LocationAdmin(admin.ModelAdmin):
     list_display = ('name','type','address')
     list_filter = ('type','city')
 
 
+# Add admin options for Events    
 class EventsAdmin(admin.ModelAdmin):
     list_display = ('name','type','address','date_time')
     list_filter = ('type','city')
@@ -25,6 +26,7 @@ admin.site.register(Location, LocationAdmin)
 admin.site.register(Event, EventsAdmin)
 
 
+# Create class to extend admin options for user profile
 class ProfileInline(admin.StackedInline):
     model = Profile
     can_delete = False
@@ -32,6 +34,7 @@ class ProfileInline(admin.StackedInline):
     fk_name = 'user'
 
 
+# Create class for new admin profile with extension included 
 class CustomUserAdmin(UserAdmin):
     inlines = (ProfileInline, )
 
@@ -40,7 +43,8 @@ class CustomUserAdmin(UserAdmin):
             return list()
         return super(CustomUserAdmin, self).get_inline_instances(request, obj)
 
-
+    
+# Replace old user admin settings with extended version
 admin.site.unregister(User)
 admin.site.register(User, CustomUserAdmin)
 
